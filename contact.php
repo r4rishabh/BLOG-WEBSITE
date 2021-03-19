5b4459ad1e63b699520ac77e8dcd691077eded83
r4rishabh <34826976+r4rishabh@users.noreply.github.com>
2021-03-19 10:38:00 +0530

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,68 @@
+<?php  include "includes/db.php"; ?>
+<?php  include "includes/header.php"; ?>
+
+
+    <!-- Navigation -->
+    
+    <?php  include "includes/navigation.php"; ?>
+
+
+
+    <?php
+
+   if(isset($_POST['submit']))
+   {
+
+      $to       = "r303799@gmail.com";
+      $subject  = wordwrap($_POST['subject'],70);
+      $body     = $_POST['body'];
+      
+      mail($to,$subject,$body);
+      
+
+    }  
+
+    ?>
+    
+ 
+    <!-- Page Content -->
+    <div class="container">
+    
+<section id="login">
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-6 col-xs-offset-3">
+                <div class="form-wrap">
+                <h1>Contact</h1>
+                    <form role="form" action="" method="post" id="login-form" autocomplete="off">
+                       
+                       
+                         <div class="form-group">
+                            <label for="email" class="sr-only">Email</label>
+                            <input type="email" name="email" id="email" class="form-control" placeholder="Enter Your Email">
+                        </div>
+                         <div class="form-group">
+                            <label for="subject" class="sr-only">Subject</label>
+                            <input type="text" name="subject" id="key" class="form-control" placeholder="Enter Your Subject">
+                        </div>
+                        <div class="form-group">
+
+                            <textarea class="form-control" name="body" id="body"></textarea>
+                            
+                        </div>
+                
+                        <input type="submit" name="submit" id="btn-login" class="btn btn-custom btn-dark  btn-lg btn-block" value="Submit">
+                    </form>
+                 
+                </div>
+            </div> <!-- /.col-xs-12 -->
+        </div> <!-- /.row -->
+    </div> <!-- /.container -->
+</section>
+
+
+        <hr>
+
+
+
+<?php include "includes/footer.php";?>
